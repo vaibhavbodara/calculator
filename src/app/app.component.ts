34d58fbf83f63ee7aba22a122300aef5c254b88f
vaibhavbodara <vaibhavbodara6110@gmail.com>
2024-06-22 10:45:39 +0530

    6/22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  // this variable declare for whatever number click on the calculator that is store in this variable
+  stringToEvaluate: string = "";
+  // calculator input method and pass parameter num
+  takeInput(num: string) {
+    this.stringToEvaluate = this.stringToEvaluate + num;
+    console.log(this.stringToEvaluate);
+  }
+
+  evaluateResult() {
+    // here we use eval() functuin for the +,-,*,/ like different operation
+    // here we write one case if user direct click = button then this give undefined for that here we use condition
+    if (this.stringToEvaluate != '') {
+      this.stringToEvaluate = eval(this.stringToEvaluate);
+    }
+
+  }
+
+  // this method is for AC button for clear the screen
+  resetResult() {
+    this.stringToEvaluate = "";
+  }
+
 }
